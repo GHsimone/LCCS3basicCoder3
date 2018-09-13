b234f074b06112ae2395d2c1c850851bab1946a4
Simone Maffei <simone.maffei@gmail.com>
2018-09-13 17:55:21 +0200

    v.1.0
    First version of the previous QGIS plugin LCCS3 Basic Coder ported to QGIS 3.*

---
+++ config.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+class config:
+    MyTitle = "LCCS3 Basic Coder 3"
+    FileFilter = "LCCS3 Legends (*.lccs);;All files (*.*)"
+    OpenLccs3legend = "Open LCCS3 legend"
+    SelectLCCS3legend = "Select a LCCS3 legend file"
+    SelectedLegend = "Selected legend"
+    NoFileSelected = "No file selected!"
+    ClassFound = "Class found"
+    ClassesLoaded = "Classes loaded"
+    AttributesLoaded = "Attributes loaded"
+    Name = "Name"
+    Description = "Description"
+    MapCode = "Map code"
+    UUID = "UUID"
+    NumberOfClasses = "Number of LCCS3 classes"
+    CodingSettings = "Coding settings"
+    FeaturesCodedWithCode = "features coded with code"
+    StartCoding = "Start coding..."
+    Coding = "...coding..."
+    NoFeatureCoded = "No feature coded"
+
+    myIface = ""
+    myNone = "<none>"
+
+    NumClasses = 0
+    LCCS3classes = []
+    NumAttributes = 0
+    CurLayerAttributes = []
+    DictLayers = dict()
+
+    LCCS3legendFile = ""
+    LCCS3layer = ""
+    LCCS3column = ""
+    LCCS3columnId = -1
+    LCCS3code = ""
